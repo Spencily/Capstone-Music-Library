39676911b7bce1c8513e3cf8160d4698f0b48634
Hannah Eves <heves.work@outlook.com>
2024-07-30 11:14:51 +0000

    Added header, footer and navbar styling for base.html template and  for the  login, signup, and logout pages

--- dettingen/urls.py
+++ dettingen/urls.py
@@ -21,4 +21,5 @@ urlpatterns = [
     path('', include("library.urls")),
     path('admin/', admin.site.urls),
     path('accounts/', include("allauth.urls")),
+    path('setlist/', include("setlist.urls")),
 ]
