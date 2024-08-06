96db1555d59816b3475e85719b0112ce219d82e4
Hannah Eves <heves.work@outlook.com>
2024-08-06 11:54:29 +0000

    Added multiselect funtionality to the Setlist Form

--- dettingen/settings.py
+++ dettingen/settings.py
@@ -57,6 +57,7 @@ INSTALLED_APPS = [
     "crispy_forms",
     "crispy_bootstrap5",
     "library",
+    'multiselectfield',
     "setlist",
 ]
 
--- setlist/forms.py
+++ setlist/forms.py
@@ -1,10 +1,14 @@
+from library.models import Piece
 from .models import Setlist
 from django import forms
 
 
 class SetForm(forms.ModelForm):
+    pieces = forms.ModelMultipleChoiceField(
+        queryset=Piece.objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+
     class Meta:
         model = Setlist
         fields = ["title", "description", "pieces"]
-
-    
